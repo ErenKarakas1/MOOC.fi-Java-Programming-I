5d570a0816eb59bdb0eb2f8bfbeebb95e955d2d1
Eren Karakaş <69261887+ErenKarakas1@users.noreply.github.com>
2020-12-13 04:27:21 +0300

    Update StarSign.java

--- Part 2/StarSign.java
+++ Part 2/StarSign.java
@@ -2,11 +2,6 @@
 public class StarSign {
 
     public static void main(String[] args) {
-
-        
-        //The tests are not checking the main, so you can modify it freely.
-        //NB: If the tests don't seem to pass, you should try the methods here
-        //in the main to make sure they print the correct shapes!
         
         printStars(3);
         System.out.println("\n---");  // printing --- between the shapes
