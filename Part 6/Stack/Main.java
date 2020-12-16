edd5394d1858f7a981dcb293316fbc67595719db
Eren Karakaş <69261887+ErenKarakas1@users.noreply.github.com>
2020-12-16 10:36:05 +0300

    Add files via upload

---
+++ Part 6/Stack/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Stack s = new Stack();
+        
+        System.out.println(s.isEmpty());
+        
+
+    }
+}
