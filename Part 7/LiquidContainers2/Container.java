a32b3d2f756a0e45ee202b7931fbaf6a0e145f1c
Eren Karakaş <69261887+ErenKarakas1@users.noreply.github.com>
2020-12-21 23:33:19 +0300

    Add files via upload

---
+++ Part 7/LiquidContainers2/Container.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Eren
+ */
+public class Container {
+    private int value;
+    
+    public Container(){
+        this.value = 0;
+    }
+    
+    public int contains(){
+        return this.value;
+    }
+    
+    public void add (int amount){
+        if (amount > 0){
+            this.value += amount;
+                if (this.value > 100){
+                    this.value = 100;
+                }
+        }
+    } 
+    
+    public void remove(int amount){
+        if (amount > 0){
+            this.value -= amount;
+        }
+        if (this.value < 0){
+            this.value = 0;
+        }
+    }
+    
+    public String toString(){
+        return this.value + "/100";
+    }
+}
+
